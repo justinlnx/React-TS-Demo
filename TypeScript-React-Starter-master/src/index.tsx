c0ab9598315d1ef0b7a82a24de6930ed3f467628
Justin Liang <justin.liang@avigilon.com>
2018-01-18 16:33:49 -0800

    configure ts and tslint and importing dependencies

--- TypeScript-React-Starter-master/src/index.tsx
+++ TypeScript-React-Starter-master/src/index.tsx
@@ -1,22 +1,31 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
+import App from './App';
 import Hello from './containers/Hello';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { enthusiasm } from './reducers/index';
 import { StoreState } from './types/index';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 
-const store = createStore<StoreState>(enthusiasm, {
-  enthusiasmLevel: 1,
-  languageName: 'TypeScript',
-});
+const windowIfDefined = typeof window === 'undefined' ? null : window as any;
+const composeEnhancers = windowIfDefined.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore<StoreState>(
+  enthusiasm,
+  {
+    enthusiasmLevel: 1,
+    languageName: 'TypeScript',
+  },
+  composeEnhancers(applyMiddleware(thunk)),
+);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Hello />
+    <BrowserRouter><App /></BrowserRouter>
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
